aa1fa0ec836c08d57111abd83ffa4581a26bd441
robin <robenjay@hotmail.nl>
2025-05-15 12:17:23 +0200

    made basic connection, should be able to casually work with the api now

--- tvm-front-end/src/App.js
+++ tvm-front-end/src/App.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://127.0.0.1:8000",
+});
 
 function App() {
-    return <h1>Hello!</h1>;
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
+
+    const handler = async (e) => {
+        e.preventDefault();
+        console.log("your question: ", question);
+        const response = await api.post("/test2", { message: question });
+        setAnswer(response.data.answer);
+    };
+
+    return (
+        <div>
+            <form>
+                <input
+                    type="text"
+                    value={question}
+                    onChange={(e) => setQuestion(e.target.value)}
+                />
+                <button type="submit" onClick={handler}>
+                    click
+                </button>
+            </form>
+            <div>
+                <h2>answer:</h2>
+                <p>{answer}</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
