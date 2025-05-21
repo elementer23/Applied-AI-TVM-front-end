9b89070aa5a3a42ca68d2081dfa3c35d171815b4
KimmyVisscher <kimmy.visscher@student.nhlstenden.com>
2025-05-21 11:08:54 +0200

    Removed the conflicted line

--- tvm-front-end/src/App.js
+++ tvm-front-end/src/App.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import Services from "./Services";
 import axios from "axios";
 import LeftSection from "./components/LeftSection";
 import RightSection from "./components/RightSection";
 import LoginScreen from "./components/LoginScreen";
+
+import React, { useState } from "react";
 
 const api = axios.create({
     baseURL: "http://127.0.0.1:8000",
@@ -20,6 +22,7 @@ function App() {
         <div className="main-container">
             <LeftSection />
             <RightSection />
+            <Services />
         </div>
     );
 }
