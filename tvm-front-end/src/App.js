41d7bcca00871d6c848d1eb1184f08b5e0f1a4c6
KimmyVisscher <kimmy.visscher@student.nhlstenden.com>
2025-05-21 12:06:29 +0200

    updated the app.js

--- tvm-front-end/src/App.js
+++ tvm-front-end/src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
 import Services from "./Services";
-import axios from "axios";
 import LeftSection from "./components/LeftSection";
 import RightSection from "./components/RightSection";
 import LoginScreen from "./components/LoginScreen";
 
 import React, { useState } from "react";
-
-const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
-});
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
