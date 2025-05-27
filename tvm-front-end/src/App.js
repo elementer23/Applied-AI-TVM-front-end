c7cb9f4043d56f6da2393bc315817de5861aab65
KimmyVisscher <kimmy.visscher@student.nhlstenden.com>
2025-05-27 16:32:21 +0200

    added the account page + functionality

--- tvm-front-end/src/App.js
+++ tvm-front-end/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginScreen from "./components/LoginScreen";
+import TextChangerScreen from "./components/TextChangerScreen";
+import AccountManager from "./components/AccountManager";
 import Main from "./Main";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
         <Routes>
             <Route path="/" element={<LoginScreen />} />
             <Route path="/main" element={<Main />} />
+            <Route path="/textChanger" element={<TextChangerScreen />} />
+            <Route path="/accountManager" element={<AccountManager />} />
         </Routes>
     );
 }
--- tvm-front-end/src/Services.js
+++ tvm-front-end/src/Services.js
@@ -1,32 +1,30 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export async function Request(question) {
-    const [answer, setAnswer] = useState("");
+    const token = localStorage.getItem("token");
 
-    //Keep in mind that the given variable in this case "input", will have to be equal to the
-    //field variable in the back-end. Meaning that if there is a class in the back-end called i don't know,
-    //InputData and it has a field called input. Then input will be the variable to send with on the front-end.
-    //And if it's called something like message, then the variable on the front-end needs to be called message too.
-    //If it doesn't equal, then it won't receive the data and it will not perform an action.. most likely resulting
-    //in an error. So keep that in mind. back-end: input -> { input: question } else
-    //back-end: message -> { message: question }.
-    const response = await api.post("/run", { input: question });
-    //The comment from above applies to what is returned as well. If you return something through a certain name like
-    //in this case "output", then it needs to be the same on the front-end as well. Or else you might send something
-    //which will work, but you won't get anything in return.
-    setAnswer(response.data.output);
-
-    console.log(answer);
+    try {
+        const response = await api.post(
+            "/run",
+            { input: question },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        return response.data.output;
+    } catch (error) {
+        console.error("Fout bij request:", error);
+        return "Er is iets misgegaan...";
+    }
 }
 
 export async function Login(requested_data, navigate) {
-    console.log(requested_data);
-
     const form = new URLSearchParams();
     form.append("username", requested_data.username);
     form.append("password", requested_data.password);
@@ -41,35 +39,25 @@ export async function Login(requested_data, navigate) {
         if (response.status === 200) {
             localStorage.setItem("token", response.data.access_token);
             navigate("/main");
+            return true;
         } else {
             console.error(response.status + " Authentication failed!");
+            return false;
         }
-
-        console.log(response.data.access_token);
     } catch (error) {
         console.error(error);
+        return false;
     }
 }
 
-export async function VerifyToken(navigate) {
-    useEffect(() => {
-        const tokenVerification = async () => {
-            const token = localStorage.getItem("token");
-            console.log(token);
+export async function VerifyToken() {
+    const token = localStorage.getItem("token");
 
-            try {
-                const response = await api.get(`/verify-token/${token}`);
-
-                if (response.status !== 200) {
-                    throw new Error("Token verification failed");
-                }
-            } catch (error) {
-                console.error(error);
-                localStorage.removeItem("token");
-                navigate("/");
-            }
-        };
-
-        tokenVerification();
-    }, [navigate]);
+    try {
+        const response = await api.get(`/verify-token/${token}`);
+        return response.status === 200;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
 }
