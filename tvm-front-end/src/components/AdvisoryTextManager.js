7fffea5cb8d3a3cbd52285adb602fab60cb01973
robin <robenjay@hotmail.nl>
2025-06-04 14:30:35 +0200

    made some changes to styling and the main page

--- tvm-front-end/src/components/AdvisoryTextManager.js
+++ tvm-front-end/src/components/AdvisoryTextManager.js
@@ -1,9 +1,49 @@
 import Header from "./Header";
+import styles from "../css/AdvisoryManager.module.css";
+import { useEffect, useState } from "react";
+import {
+    GetAllCategories,
+    GetAllSubcategoriesByCategory,
+} from "../utils/Services";
 
 function AdvisoryTextManager() {
+    const [categories, setCategories] = useState([]);
+    const [selectedKey, setSelectedKey] = useState(null);
+
+    useEffect(() => {
+        async function fetchAllCategories() {
+            const data = await GetAllCategories();
+            const categoryData = data.current_response;
+            setCategories(categoryData);
+
+            if (categoryData.length > 0) setSelectedKey(categoryData[0].id);
+        }
+
+        fetchAllCategories();
+    }, []);
+
+    console.log(selectedKey);
+
     return (
         <div className="section right-section">
             <Header />
+            <div className={styles.advisoryManagerHeader}>
+                <h3 className={styles.advisoryManagerHeading}>
+                    Advies overzicht
+                </h3>
+            </div>
+            <div className={styles.advisoryManagerSection}>
+                {categories &&
+                    categories.map((category) => (
+                        <div
+                            key={category.id}
+                            className={styles.advisoryManagerCategoryPlatform}
+                            onClick={() => setSelectedKey(category.id)}
+                        >
+                            {category.name}
+                        </div>
+                    ))}
+            </div>
         </div>
     );
 }
