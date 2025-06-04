cd69bf741349ccb6371bbf413ad9f7fdbadc4d19
robin <robenjay@hotmail.nl>
2025-06-04 15:22:39 +0200

    added more styling to the front-end, for showing categories. Made the general page better

--- tvm-front-end/src/components/AdvisoryTextManager.js
+++ tvm-front-end/src/components/AdvisoryTextManager.js
@@ -9,20 +9,26 @@ import {
 function AdvisoryTextManager() {
     const [categories, setCategories] = useState([]);
     const [selectedKey, setSelectedKey] = useState(null);
+    const [subcategories, setSubcategories] = useState([]);
 
     useEffect(() => {
         async function fetchAllCategories() {
             const data = await GetAllCategories();
             const categoryData = data.current_response;
             setCategories(categoryData);
-
-            if (categoryData.length > 0) setSelectedKey(categoryData[0].id);
         }
 
         fetchAllCategories();
     }, []);
 
-    console.log(selectedKey);
+    useEffect(() => {
+        async function fetchSubcategories() {
+            const data = await GetAllSubcategoriesByCategory(selectedKey);
+            if (data.success) setSubcategories(data.current_response);
+        }
+
+        fetchSubcategories();
+    }, [selectedKey]);
 
     return (
         <div className="section right-section">
@@ -35,12 +41,29 @@ function AdvisoryTextManager() {
             <div className={styles.advisoryManagerSection}>
                 {categories &&
                     categories.map((category) => (
-                        <div
-                            key={category.id}
-                            className={styles.advisoryManagerCategoryPlatform}
-                            onClick={() => setSelectedKey(category.id)}
-                        >
-                            {category.name}
+                        <div key={category.id}>
+                            <div
+                                className={
+                                    styles.advisoryManagerCategoryPlatform
+                                }
+                                onClick={() => setSelectedKey(category.id)}
+                            >
+                                {category.name}
+                            </div>
+                            {selectedKey === category.id && (
+                                <div>
+                                    {subcategories.map((subcategory) => (
+                                        <div
+                                            key={subcategory.id}
+                                            className={
+                                                styles.advisoryManagerSubcategoryPlatform
+                                            }
+                                        >
+                                            {subcategory.name}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
                         </div>
                     ))}
             </div>
