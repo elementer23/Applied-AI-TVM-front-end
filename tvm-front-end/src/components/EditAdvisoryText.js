ab78aae287b613b303d48d485fe4ae2a022f72f1
Road-Train <trol13xd@gmail.com>
2025-06-04 11:20:43 +0200

    First dropdown finally done. Time for a 2nd one.

--- tvm-front-end/src/components/EditAdvisoryText.js
+++ tvm-front-end/src/components/EditAdvisoryText.js
@@ -1,9 +1,47 @@
 import Header from "./Header";
+import React, { useEffect, useState } from 'react';
+import {GetAllCategories} from "../utils/Services";
 
 function EditAdvisoryText() {
+    const [categories, setCategories] = useState([]);
+    const [selectedCategoryId, setSelectedCategoryId] = useState('');
+
+    useEffect(() =>
+    {
+        async function fetchCategories()
+        {
+            const result = await GetAllCategories();
+
+            if (result.success)
+            {
+                console.log(result.current_response); // Debug
+                setCategories(result.current_response);
+
+                const firstId = result.current_response[0]?.id;
+                setSelectedCategoryId(firstId);
+            }
+        }
+        fetchCategories();
+    }, []);
+
+    const handleChange = (e) =>
+    {
+        setCategories(e.target.value);
+    };
+
     return (
-        <div className="section right-section">
-            <Header />
+        <div>
+            <label htmlFor="categories">Category:</label>
+            <select
+                value={selectedCategoryId || ''}
+                onChange={(e) => setSelectedCategoryId(parseInt(e.target.value))}
+            >
+                {categories.map((category) => (
+                    <option key={category.id} value={category.id}>
+                        {category.name}
+                    </option>
+                ))}
+            </select>
         </div>
     );
 }
